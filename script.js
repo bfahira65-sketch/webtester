312c76e853f682d92e1176f6a53ab5ac00cb72f7
bfahira65-sketch <bfahira65@gmail.com>
2026-07-10 01:43:40 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector(".login").onclick=function(){
+
+alert("Menu Login akan dibuat pada Tahap 2");
+
+}
+
+document.querySelector(".register").onclick=function(){
+
+alert("Menu Daftar akan dibuat pada Tahap 2");
+
+}
+
+window.addEventListener("scroll",()=>{
+
+document.querySelector("header").style.background="rgba(0,0,0,.95)";
+
+});
